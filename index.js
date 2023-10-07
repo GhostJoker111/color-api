42b64bf0be9ad70ba00cb962e0b3fcf9db02f941
Артем Журкин <artyom.jurkin@yandex.ru>
2023-10-07 21:44:35 +0300

    Add initial composition

---
+++ index.js
@@ -0,0 +1,7 @@
+const colorSectionEl = document.getElementById("color-section");
+const colorSettingsEl = document.getElementById("color-settings");
+
+function renderColors() {
+  const rgbPaletteEl = document.querySelector("rgb-palette");
+  console.log(rgbPaletteEl)
+}
